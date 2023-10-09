82d8d58394a2c321de7fd7bc8e7f445e75ad0720
Mykhailo Fomenko <misha.fomenko00@gmail.com>
2023-10-08 20:36:39 -0400

    added local storage functionality for the cart state

--- app/context/cartContext.jsx
+++ app/context/cartContext.jsx
@@ -1,10 +1,22 @@
 'use client';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 const CartContext = createContext({});
 export const CartContextProvider = ({ children }) => {
+
     const [cart, setCart] = useState([]);
     const [cartOpen, setCartOpen] = useState(false);
+
+    useEffect(() => {
+        try {
+            const initJson = localStorage.getItem('cart');
+            const initCart = JSON.parse(initJson);
+            initCart && !cart.length && setCart(initCart);
+        } catch {
+            console.log('Unable to use localStorage on the server');
+        };
+        cart.length && localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart, setCart])
 
     return (
         <CartContext.Provider value={{ cart, setCart, cartOpen, setCartOpen }}>
